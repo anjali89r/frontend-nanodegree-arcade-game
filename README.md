f7b1e80e11360e5b98605ccdc32838fa936487df
anjali89r <anjeng1989@gmail.com>
2018-06-26 15:02:12 -0400

    Ready for review

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
-frontend-nanodegree-arcade-game
-===============================
+#frontend-nanodegree-arcade-game
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+DESCRIPTION
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+In the game, goal of player is to reach water without colliding into any of enemies. This project has basic game functionality but covers advanced Object-Oriented JavaScript concepts and HTML5 Canvas.
+
+## Instructions
+
+* Needs browser to run the application
+* Opening the index.html file loads the application.
+* chrome dev tools preferred for testing
+
+## How to Play
+
+* Use arrow keys to move up-down, left-right.
+* Bugs should be avoided, if the player collides with the bug, the player's score is reduced and has to begin from the starting position.
+* Player wins upon reaching the water and the game restarts.
+
+## Whats Included
+
+1) style.css
+2) images file which has the different characters that can be used in the game.
+3) js/app.js contains the js code handling the functionality of the game.
+4) js/engine.js - contains game loop functionality - draws the initial game board on the screen, - calls update, render methods on player and enemy objects.
+5) js/resource.js - it is a simple image loading utility
+6) index.html file
--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(xAxis, yAxis) {
     this.sprite = 'images/enemy-bug.png';
+    this.x= xAxis;
+    this.y = yAxis;
+    this.speed =  Math.floor((Math.random() * 130)) + 100;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+//update enemy's position. dt is the time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
+    // multiplying any movement by the dt parameter
+    // ensures the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    if (this.x > 505) {
+        this.x = 0;
+        this.speed =  Math.floor((Math.random() * 100)) + 100;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,19 +23,111 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//Score class - increases by 100 if the player reaches on water, decreases by 50 when collided by bug
+var Score = function() {
+    this.x = 400;
+    this.y = 580;
+    this.score = 0;
+}
+ //referred from mdn docs: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillText
+Score.prototype.render = function() {
+    ctx.font = '30px serif';
+    ctx.fillStyle = '#FFEB3B';
+    ctx.textAlign = 'right';
+    ctx.fillText('Score: ' + this.score, this.x, this.y);
+};
+let score = new Score();
+
+//Player class
+var Player = function(playerXaxis, playerYaxis) {
+    this.sprite = 'images/char-boy.png';
+    this.x = playerXaxis;
+    this.y = playerYaxis;
+}
+
+//method implemented for detecting collision by checking the difference between the distance of the player and bug: hyp2 = x2 + y2
+Player.prototype.collisionDetection = function(enemyBug) {
+    let xAxisDiff = enemyBug.x - this.x;
+    let yAxisDiff = enemyBug.y - this.y;
+    let distanceDiff = Math.sqrt((xAxisDiff * xAxisDiff) + (yAxisDiff * yAxisDiff));
+    if (distanceDiff < 50) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+//reset method brings back the player to its starting position
+Player.prototype.reset = function() {
+    this.x = 202;
+    this.y = 405;
+}
+
+//method to see if player collided with any of the enemies and reducing the score
+Player.prototype.checkCollission = function() {
+    for (let enemy of allEnemies) {
+        if (this.collisionDetection(enemy)) {
+            this.y = 405;
+            score.score -= 50;
+            return true;
+        }
+    }
+}
+
+//check for collission to update
+Player.prototype.update = function() {
+    this.checkCollission();
+}
+//method for moving player as per the keyPress.
+Player.prototype.handleInput = function (keyPress) {
+
+    if (keyPress === 'left' && this.x > 0) {
+        this.x -= 101;
+    }
+
+    if (keyPress === 'right' && this.x < 400) {
+        this.x += 101;
+    }
+
+    if (keyPress === 'up' && this.y > -10) {
+        this.y -= 83;
+    }
+
+    if (keyPress === 'down' && this.y < 405) {
+        this.y += 83;
+    }
+//player reaches on top of water increases score by 100
+    if (this.y <= -10) {
+        score.score += 100;
+        let actualThis = this;
+        setTimeout(function () {
+            actualThis.reset();
+            score.score = 0;
+        }, 900);
+
+    }
+    if (this.y > 430) {
+            this.reset();
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Now instantiating objects.
 
+let enemyOne = new Enemy(((Math.random() * 275) + 45), 140);
+let enemyTwo = new Enemy(((Math.random() * 140) + 35), 220);
+let enemyThree = new Enemy(((Math.random() * 315) + 15), 300);
+let enemyFour = new Enemy(((Math.random() * 215) + 25), 70);
+let allEnemies = [enemyOne, enemyTwo, enemyThree, enemyFour];
+let player = new Player(200, 405);
 
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
